67e2d42f5fb7d67358bc417577e2d1cd19c13d6b
tsvetiivanova <64466346+tsvetiivanova@users.noreply.github.com>
2020-05-02 19:06:03 +0300

    Update DeterminateFiniteAutomaton.hpp

--- DeterminateFiniteAutomaton.hpp
+++ DeterminateFiniteAutomaton.hpp
@@ -11,8 +11,28 @@
 // typedef std::vector<std::vector<State>> TransitionTable;
 
 template <typename T> class DeterminateFiniteAutomaton {
+    public:
+    friend std::ostream & operator<<(std::ostream&, const DeterminateFiniteAutomaton <T> &);
+	friend std::istream& operator>>(std::istream &, DeterminateFiniteAutomaton<T> &);
+	DeterminateFiniteAutomaton();
+	DeterminateFiniteAutomaton(std::vector<T>, std::vector<State>, std::vector<std::vector<State>>, char , std::vector<State>);
+	DeterminateFiniteAutomaton(std::vector<std::vector<State>>);
+	DeterminateFiniteAutomaton(std::vector<State>);
+	
+
+	DeterminateFiniteAutomaton(const DeterminateFiniteAutomaton &);
+	~DeterminateFiniteAutomaton();
+	DeterminateFiniteAutomaton& operator=(const DeterminateFiniteAutomaton&);
+    private:
     std::vector<T> alphabet;
     std::vector<State> states;
+	std::vector<std::vector<State>> TransitionTable;
+	std::vector<State> FinalState; 
+	unsigned countState;
+	unsigned countAlphabet; 
 };
-
+template <typename T>
+std::ostream& operator<<(std::ostream &, const DeterminateFiniteAutomaton<T> &);
+template <typename T>
+std::istream& operator>>(std::istream &, DeterminateFiniteAutomaton<T> &);
 #endif
