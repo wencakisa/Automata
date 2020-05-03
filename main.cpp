493606c0fb931ad51620f712f73ad3973041fdc8
Ventsislav Tashev <wencakisa@gmail.com>
2020-05-03 16:49:16 +0300

    Automaton type based on input integer

--- main.cpp
+++ main.cpp
@@ -4,7 +4,25 @@
 
 int main() {
     try {
-        DeterminateFiniteAutomaton<char> automaton;
+        int alphabetType;
+
+        std::cout << "Choose an alphabet type (1 - int, 2 – char): ";
+        std::cin >> alphabetType;
+
+        if (alphabetType == 1) {
+            DeterminateFiniteAutomaton<int> *automaton = new DeterminateFiniteAutomaton<int>();
+
+            std::cin >> *automaton;
+            std::cout << *automaton;
+        } else if (alphabetType == 2) {
+            DeterminateFiniteAutomaton<char> *automaton = new DeterminateFiniteAutomaton<char>();
+
+            std::cin >> *automaton;
+            std::cout << *automaton;
+        } else {
+            throw std::runtime_error("Unknown alphabet type.");
+        }
+
     } catch (const std::exception& e) {
         std::cout << e.what() << std::endl;
     }
